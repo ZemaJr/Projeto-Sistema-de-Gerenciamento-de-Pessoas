531e938c30a71e95a380d1207276163de581cc99
ZemaJr <zema.eng.el@gmail.com>
2021-08-30 17:02:04 -0300

    Commit001

---
+++ src/main/java/one/digitalinnovation/developerapi/entity/Phone.java
@@ -0,0 +1,29 @@
+package one.digitalinnovation.developerapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.developerapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    //Automatically assigns an id value when uploading to database
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private  Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+}
